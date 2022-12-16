16ef35b12764a6eb405f67211601255f81a26fa6
Karolinegaia <drakarolinegaia@gmail.com>
2022-12-16 14:55:40 -0300

    Ajustando o Search

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -1,18 +1,42 @@
-import {Link} from "react-router-dom"
+import { useState } from "react";
+import {Link, useNavigate} from "react-router-dom"
 import {BiCameraMovie, BiSearchAlt2} from "react-icons/bi"
 import "./NavBarStyle.css"
 
+
 export function NavBar() {
-  <nav id="NavBar">
-    <h2>
-      <Link to="/">
-        <BiCameraMovie />
-        Movie Karol
-      </Link>
-    </h2>
-    <form type="text" placeholder="Busque um filme" />
-    <button type="submit">
-      <BiSearchAlt2 />
-    </button>
-  </nav>;
-}
+  const [search, setSearch] = useState("");
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (!search) return;
+
+    navigate(`/search?q=${search}`, { replace: true });
+    setSearch("");
+  };
+
+
+  return (
+    <nav id="navbar">
+      <h2>
+        <Link to="/">
+          <BiCameraMovie /> MoviesLib
+        </Link>
+      </h2>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Busque um filme"
+          onChange={(e) => setSearch(e.target.value)}
+          value={search}
+        />
+        <button type="submit">
+          <BiSearchAlt2 />
+        </button>
+      </form>
+    </nav>
+  );
+};
+
